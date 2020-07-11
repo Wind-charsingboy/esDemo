9c66db965eeb546654cfe7e3a66e986fd2ebede3
yinhongchao <yinhongchao@ijovo.com>
2020-07-11 10:34:08 +0800

    测试

---
+++ src/main/java/com/example/demo/controller/DevController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+/**
+ * @author yinhongchao
+ * @date 2020/7/11 10:30
+ */
+public class DevController {
+
+    public void testController() {
+        System.out.println("kkkkkkkkk");
+    }
+}
